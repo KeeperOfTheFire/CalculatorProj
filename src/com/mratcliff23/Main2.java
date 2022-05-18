180a38b500d79f4dabb6ab5cd84ff50d39ac57e1
KeeperOfTheFire <mwratcliff@gmail.com>
2022-05-18 09:26:18 -0700

    idek TBH

--- src/com/mratcliff23/Main2.java
+++ src/com/mratcliff23/Main2.java
@@ -1,10 +1,14 @@
 package com.mratcliff23;
 
+import javax.sound.midi.SysexMessage;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main2 {
 
     static Scanner in = new Scanner(System.in);
+
+    public static ArrayList<Result> ans = new ArrayList();
 
     public static void main(String[] args) {
        String input = "";
@@ -12,10 +16,10 @@ public class Main2 {
             System.out.print("enter your input, it can be an expression like '1+1', or a command like 'ans' or 'mat': ");
             input = in.nextLine();
             Parser parsed = new Parser(input);
-            if(parsed.isExpression()){
 
-            }else if(parsed.containMatrix()) {
+            if(parsed.containMatrix()){
                 int rows, cols;
+                System.out.println("Initializing Matrix 'MatA':");
                 System.out.print("enter the number of rows for the Matrix " + parsed.getParsed()[0] + ": ");
                 rows = in.nextInt();
                 in.nextLine();
@@ -23,11 +27,30 @@ public class Main2 {
                 cols = in.nextInt();
                 in.nextLine();
 
-                Matrix mat1 = new Matrix(rows, cols, parsed.getParsed()[0]);
-                System.out.println("now filling the matrix...");
-                mat1.fillMatrix();
+                Matrix matA = new Matrix(rows, cols, parsed.getParsed()[0]);
+                System.out.println("now filling 'MatB'...");
+                matA.fillMatrix();
+
+
+                System.out.println("Initializing Matrix 'MatB':");
+                System.out.print("enter the number of rows for the Matrix " + parsed.getParsed()[0] + ": ");
+                rows = in.nextInt();
+                in.nextLine();
+                System.out.print("\n" + "enter the number of columns for the matrix " + parsed.getParsed()[0] + ": ");
+                cols = in.nextInt();
+                in.nextLine();
+
+                Matrix matB = new Matrix(rows, cols, parsed.getParsed()[0]);
+                System.out.println("now filling 'MatB'...");
+                matB.fillMatrix();
+
+
+
+            }else if(parsed.matrixOperations()) {
+                ans.add(parsed.matrixEvaluate());
+                System.out.println(ans.get(ans.size()-1));
             }else{
-                System.out.println("This input is not an expression");
+                System.out.println("This input is invalid");
             }
 
 
--- src/com/mratcliff23/Parser.java
+++ src/com/mratcliff23/Parser.java
@@ -23,11 +23,32 @@ public class Parser {
 
     public boolean containMatrix(){
         for(String n : parsed){
-            if(n.toLowerCase().indexOf("mat") >= 0){
-                return true;
+            switch(n.toLowerCase()){
+                case "matrix":
+                case "mat":
+                    return true;
             }
         }
         return false;
+    }
+
+    public boolean matrixOperations(){
+        for(int i = 1; i < parsed.length-1; i++){
+            if(parsed[i].equals("*") || parsed[i].equals("/") || parsed[i].equals("+") || parsed[i].equals("-")){
+                if(parsed[i-1].equalsIgnoreCase("mata")  || parsed[i+1].equalsIgnoreCase("mata")) {
+                    return true;
+                }else if(parsed[i-1].equalsIgnoreCase("matb")  || parsed[i+1].equalsIgnoreCase("matab")){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public Matrix matrixEvaluate(){
+        Matrix result = new Matrix(1, 1);
+
+        return result;
     }
 
 
